07339b8f77d71d9d9afa2e56072cee7129efc76d
Simone <101086867+GentiliSimone@users.noreply.github.com>
2022-06-15 19:27:02 +0200

    Update images.py

--- imgPackage/images.py
+++ imgPackage/images.py
@@ -1,6 +1,17 @@
 from imgPackage import png
 
+'''
+    It allows you to analyze and edit and save images 
+    with png extension.
+'''
+
 def load(filename):
+    
+    '''
+        Returns a matrix containing the RGB tuples 
+        of the specified image.
+    '''
+    
     with open(filename, mode='rb') as f:
         reader = png.Reader(file=f)
         w, h, png_img, _ = reader.asRGB8()
@@ -10,6 +21,12 @@ def load(filename):
                  for line in png_img ]
     
 def save(img, filename):
+    
+    '''
+        Saves the specified matrix (made up of tuples 
+        containing RGB channels) in the specified path.
+    '''
+    
     pngimg = png.from_array(img,'RGB')
     pngimg.save(filename)
 
